7432e3069448f31526d440baa40d04d1d5aeb2ed
Taegeon Go (Alan) <tae.gun7784@gmail.com>
2024-10-17 16:46:30 +0900

    update texts

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Time to use this template to create a new React app.
         </p>
         <a
           className="App-link"
